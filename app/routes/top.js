ea65a673652e7156b39e9b0a0f5e41148bcde569
Roberto Sequeira <roberto@robertosequeira.info>
2020-12-08 23:56:27 -0600

    Display top 100 urls

--- app/routes/top.js
+++ app/routes/top.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class TopRoute extends Route {
+  async model() {
+    // TODO: Extract URL
+    const response = await fetch('http://localhost:3000/');
+    const { urls } = await response.json();
+
+    return urls;
+  }
 }
