5f26c4508145d013a8c6a1aa8749c797b422a7e7
Roberto Sequeira <roberto@robertosequeira.info>
2020-12-10 00:25:08 -0600

    Restore single quotes removed by VS Code plugin

--- app/components/shortener.js
+++ app/components/shortener.js
@@ -1,32 +1,32 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
-import validateFormat from "ember-validators/format";
-import { NotFoundError, InvalidError } from "@ember-data/adapter/error";
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import validateFormat from 'ember-validators/format';
+import { NotFoundError, InvalidError } from '@ember-data/adapter/error';
 
 export default class ShortenerComponent extends Component {
   @service store;
 
-  @tracked url = "";
-  @tracked shortCode = "";
-  @tracked invalid = "";
+  @tracked url = '';
+  @tracked shortCode = '';
+  @tracked invalid = '';
   invalidUrlMessage = "Please enter a valid url. Don't forget to include https:// or http://. Ex: https://facebook.com";
 
   @action
   shortenUrl(event) {
     event.preventDefault();
 
-    this.shortCode = "";
-    this.invalid = "";
+    this.shortCode = '';
+    this.invalid = '';
 
-    if (validateFormat(this.url, { type: "url" }) === true) {
-      let newUrl = this.store.createRecord("url", { full_url: this.url });
+    if (validateFormat(this.url, { type: 'url' }) === true) {
+      let newUrl = this.store.createRecord('url', { full_url: this.url });
 
       newUrl
         .save()
         .then((url) => {
-          this.url = "";
+          this.url = '';
           this.shortCode = url.short_code;
         })
         .catch((error) => {
@@ -36,11 +36,11 @@ export default class ShortenerComponent extends Component {
           }
 
           if (error instanceof NotFoundError) {
-            this.invalid = "Cannot contact the server";
+            this.invalid = 'Cannot contact the server';
             return;
           }
 
-          this.invalid = "Cannot process the request";
+          this.invalid = 'Cannot process the request';
         });
     } else {
       this.invalid = this.invalidUrlMessage;
